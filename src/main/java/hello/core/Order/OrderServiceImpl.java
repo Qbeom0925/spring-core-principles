a9dfe9c273a0ec8e4656f8fd2602791f22763f3b
kyubeom <be_man@naver.com>
2022-06-26 21:11:16 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -3,22 +3,38 @@ package hello.core.Order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPlicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
+//    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPlicy) {
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("discountPlicy = " + discountPlicy);
         this.memberRepository = memberRepository;
-        this.discountPlicy = discountPlicy;
+        this.discountPolicy = discountPlicy;
+    }
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
     }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member= memberRepository.findById(memberId);
-        int discountPrice = discountPlicy.discount(member, itemPrice);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
