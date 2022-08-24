c2dad67200743c92d41646ec52ec570420a73512
kyubeom <be_man@naver.com>
2022-08-24 15:51:06 +0900

    강의 완강

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -10,6 +10,8 @@ public class SingletonService {
 
     private SingletonService(){
 
+
+
     }
 
     public void logic(){
--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -6,11 +6,11 @@ public class StatefulService {
 
     public int order(String name, int price){
         System.out.println("name = " + name + "price = "+price);
-//        this.price= price;//여기가 문제
+        this.price= price;//여기가 문제
         return price;
     }
 
-//    public int getPrice(){
-//        return price;
-//    }
+    public int getPrice(){
+        return price;
+    }
 }
